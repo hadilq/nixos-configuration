9f4d59f09fadca717dde972e79d85106cef85b0b
Hadi <hadilashakr@gmail.com>
2022-09-17 17:10:57 -0400

    Accelerated Video Playback

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -65,4 +65,18 @@
   };
 
   hardware.bluetooth.enable = true;
+
+  # Accelerated Video Playback
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 }
