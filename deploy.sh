5f5b2ad9293602b2068c52ee4fd05c31ec393656
Hadi <hadilq.dev@gmail.com>
2025-02-14 12:46:36 -0500

    make it simple!

--- deploy.sh
+++ deploy.sh
@@ -20,23 +20,12 @@ if [ ! -d "$OUTPUT_DIR/.git" ]; then
   sudo rm -rf $OUTPUT_DIR
   sudo git clone $ORIGIN $OUTPUT_DIR
   sudo git checkout -b $LOCAL_CHANGES_BRANCH
-  echo "cloning completed, but you need to commit users.nix and network.nix files manually!" >&2
-  exit 2;
 fi
-
-LOCAL_COMMIT=`sudo git show --format="%H" --no-patch`
-echo "local commit is $LOCAL_COMMIT" >&2
 
 sudo rm flake.lock
 sudo git config --global user.email "hadilq.dev@gmail.com"
 sudo git config --global user.name "Hadi"
-sudo git branch -D main
-sudo git fetch -a
-sudo git checkout -f main # In case of `commit --ammend` this works, but not `pull --rebase`
-sudo git branch -D $LOCAL_CHANGES_BRANCH
-sudo git checkout -b $LOCAL_CHANGES_BRANCH # creating this branch on the HEAD of main now
-sudo git clean -fdx
-sudo git cherry-pick $LOCAL_COMMIT
+sudo git pull --rebase origin $LOCAL_CHANGES_BRANCH
 
 sudo rm -rf $BACKUP_DIR
 sudo cp -a $OUTPUT_DIR $BACKUP_DIR
