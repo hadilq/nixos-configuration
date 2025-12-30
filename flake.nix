c82cb051fa684071244bc77e13cda699421bbc02
Hadi <hadilq.dev@gmail.com>
2025-12-29 22:54:23 -0500

    remove cosmic and unstable inputs

--- flake.nix
+++ flake.nix
@@ -2,18 +2,13 @@
   inputs = {
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3.13";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
-    nixpkgs-master.url = "github:nixos/nixpkgs/dbc2252f27e4125f5ceb8bdaa0091d3e4d78edd0";
-    #nixpkgs-master.follows = "nixos-cosmic/nixpkgs";
-    nixos-cosmic = {
-      url = "github:lilyinstarlight/nixos-cosmic";
-    };
     microvm = {
       url = "github:astro/microvm.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, determinate, nixpkgs, nixpkgs-master, nixos-cosmic, microvm, ... }@attrs:
+  outputs = { self, determinate, nixpkgs, microvm, ... }@attrs:
   {
     nixosConfigurations.darter = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
